8d77f7aae972b2b4fc2ec0b6f98e0044f2a562a2
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-02-16 19:37:57 +0530

    refactor 1 done

--- FindMaxTest.java
+++ FindMaxTest.java
@@ -2,69 +2,112 @@ import org.example.FindMax;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
-public class FindMaxTest {
+public class FindMaxTest<T extends Comparable> {
     FindMax obj = new FindMax();
-    @Test
-    public void findMaxTest1(){
+//    @Test
+//    public void findMaxTest1(){
+//
+//     int expected=   obj.findMax(40,30,20);
+//     assertEquals(expected,40);
+//
+//    }
+//    @Test
+//    public void findMaxTest2(){
+//
+//        int expected=   obj.findMax(30,40,20);
+//        assertEquals(expected,40);
+//
+//    }
+//    @Test
+//    public void findMaxTest3(){
+//
+//        int expected=   obj.findMax(20,30,40);
+//        assertEquals(expected,40);
+//
+//    }
+//
+//    @Test
+//    public void findMaxFloatTest1(){
+//
+//        float expected=   obj.findMaxFloat(40.0f,30.0f,20.0f);
+//        assertEquals(expected,40.0f);
+//
+//    }
+//    @Test
+//    public void findMaxFloatTest2(){
+//
+//        float expected=   obj.findMaxFloat(30.0f,40.0f,20.0f);
+//        assertEquals(expected,40.0f);
+//
+//    }
+//    @Test
+//    public void findMaxFloatTest3(){
+//
+//        float expected=   obj.findMaxFloat(20.0f,40.0f,30.0f);
+//        assertEquals(expected,40.0f);
+//
+//    }
+//    @Test
+//    public void findlongestStringTest1(){
+//
+//        String expected=   obj.findLongestString("abcd","ab","abc");
+//        assertEquals(expected,"abcd");
+//
+//    }
+//    @Test
+//    public void findlongestStringTest2(){
+//
+//        String expected=   obj.findLongestString("abc","abcd","ab");
+//        assertEquals(expected,"abcd");
+//
+//    }
+//    @Test
+//    public void findlongestStringTest3(){
+//
+//        String expected=   obj.findLongestString("ab","abc","abcd");
+//        assertEquals(expected,"abcd");
+//
+//    }
 
-     int expected=   obj.findMax(40,30,20);
+    @Test
+    public <T extends Comparable> void findMaxTest1(){
+
+     Comparable expected=   obj.findMax(40,30,20);
      assertEquals(expected,40);
 
     }
     @Test
-    public void findMaxTest2(){
+    public <T extends Comparable> void findMaxTest2(){
 
-        int expected=   obj.findMax(30,40,20);
-        assertEquals(expected,40);
-
-    }
-    @Test
-    public void findMaxTest3(){
-
-        int expected=   obj.findMax(20,30,40);
-        assertEquals(expected,40);
-
-    }
-
-    @Test
-    public void findMaxFloatTest1(){
-
-        float expected=   obj.findMaxFloat(40.0f,30.0f,20.0f);
-        assertEquals(expected,40.0f);
+        Comparable expected=   obj.findMax(20,30,40);
+     assertEquals(expected,40);
 
     }
     @Test
-    public void findMaxFloatTest2(){
+    public <T extends Comparable> void  findMaxTest3(){
 
-        float expected=   obj.findMaxFloat(30.0f,40.0f,20.0f);
-        assertEquals(expected,40.0f);
-
-    }
-    @Test
-    public void findMaxFloatTest3(){
-
-        float expected=   obj.findMaxFloat(20.0f,40.0f,30.0f);
-        assertEquals(expected,40.0f);
+        Comparable expected=   obj.findMax(30,40,20);
+     assertEquals(expected,40);
 
     }
     @Test
-    public void findlongestStringTest1(){
+    public <T extends Comparable> void  findMaxStringTest1(){
 
-        String expected=   obj.findLongestString("abcd","ab","abc");
+        Comparable expected=   obj.findMax("ab","abc","abcd");
         assertEquals(expected,"abcd");
 
     }
     @Test
-    public void findlongestStringTest2(){
+    public <T extends Comparable> void  findMaxStringTest2(){
 
-        String expected=   obj.findLongestString("abc","abcd","ab");
+        Comparable expected=   obj.findMax("abcd","a","abc");
         assertEquals(expected,"abcd");
 
     }
     @Test
-    public void findlongestStringTest3(){
+    public <T extends Comparable> void  findMaxStringTest3(){
 
-        String expected=   obj.findLongestString("ab","abc","abcd");
+        Comparable expected=   obj.findMax("a","abcd","ab");
         assertEquals(expected,"abcd");
 
     }
